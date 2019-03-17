bd5b2ecd7e9ede0eabba5306170a1611216de47a
Öyvind <oybo84@gmail.com>
2019-03-17 14:12:51 +0100

    Constructor and getters added to parser class

--- src/main/java/RequestParser.java
+++ src/main/java/RequestParser.java
@@ -1,4 +1,51 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 public class RequestParser {
 
+    private String fileRequested;
+    private String requestType;
+    private ArrayList<String> formData;
+    private String contentLength;
+    private String params;
+    private String plainFileName;
+    private InetAddress ip;
 
+    public RequestParser(String s, InetAddress ip) {
+        this.fileRequested = fileRequested;
+        this.requestType = requestType;
+        this.formData = formData;
+        this.contentLength = contentLength;
+        this.params = params;
+        this.plainFileName = plainFileName;
+        this.ip = ip;
+    }
+
+    public String getFileRequested() {
+        return fileRequested;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public ArrayList<String> getFormData() {
+        return formData;
+    }
+
+    public String getContentLength() {
+        return contentLength;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public String getPlainFileName() {
+        return plainFileName;
+    }
+
+    public InetAddress getIp() {
+        return ip;
+    }
 }
